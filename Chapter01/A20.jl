a179cecebe6ce450125f0e233ec7fe012766cfff
Lucas Neumann <lucas95n@gmail.com>
2019-08-14 20:33:22 -0300

    style

--- Chapter01/A20.jl
+++ Chapter01/A20.jl
@@ -1,16 +1,17 @@
-function numeric_sol(AO, BO, time, dt=0.1, k1=0.1)
+#Solução para A20
+function numerical_sol(AO, BO, time, dt=0.1, k1=0.1)
     nsteps = Int64(time/dt)
     T = collect(range(0, length=nsteps, step=Float64(dt)))
     A = Vector{Float64}(undef,nsteps); A[1] = AO
     B = Vector{Float64}(undef,nsteps); B[1] = BO
     for ts in 1:(nsteps -1)
-        if A[ts] < 1.e-5
+        A[ts+1] = A[ts] - k1 * dt * A[ts]
+        B[ts+1] = B[ts] + k1 * dt * A[ts]
+        if A[ts] < 1.e-6
             A[ts+1] = 0.
-        elseif B[ts] < 1.e-5
+        elseif B[ts] < 1.e-6
             B[ts+1] = 0.
         end
-        A[ts+1] = A[ts] - k1 * dt * A[ts]
-        B[ts+1] = A[ts] + k1 * dt * A[ts]
     end
     @time T, A, B
 end
@@ -28,5 +29,5 @@ function analitical_sol(AO, BO, time, dt=0.1, k1=0.1)
 end
 
 
-numeric_sol(100,0,100)
+numerical_sol(100,0,100)
 analitical_sol(100,0,100)
